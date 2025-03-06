a173207cd9b050678b85bc3372a3f9769041794f
ADRIANBG2325 <adrianbautista046@gmail.com>
2025-03-05 18:56:06 -0600

    ppp

---
+++ Documents/Progamacio java/Herencia/src/Panda.java
@@ -0,0 +1,9 @@
+public class Panda  {
+  
+    public static void main(String[] args) {
+   SuperC obj= new SuperC ();
+   obj.bienvenido();
+   Jirafa obj2 =new Jirafa();
+   obj2 .interaccion ();
+           }
+}
