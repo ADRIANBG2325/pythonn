935de382f3e930aabbace53029ee13e8d6ece792
ADRIANBG2325 <adrianbautista046@gmail.com>
2025-03-09 18:58:53 -0600

    gg

---
+++ Documents/Progamacio java/Herencia/src/herencia/Final.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author adrian
+ */
+class Final {
+     JOptionPane mensaje;
+    public void Final(){
+    mensaje.showMessageDialog(null,"oda");
+    }
+}
