34e9fffeb795d81809a35984b98c691490787a44
ADRIANBG2325 <adrianbautista046@gmail.com>
2025-03-11 19:01:14 -0600

    GG

---
+++ Documents/Progamacio java/Herencia/src/herencia/Proyecto.java
@@ -0,0 +1,27 @@
+package herencia;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+public class Proyecto {
+public static void main(String[] args) {
+Ventanas a = new Ventanas();
+a.setSize(500,200);
+a.setResizable(false);
+a.setLocationRelativeTo(null);
+a.getContentPane().setBackground(Color.white);
+a.setVisible(true);
+/*JTextField jtusuario =new JTextField("nomre");
+       jtusuario.setBounds(30,80,200,20);
+        
+       JTextField pspasword =new JTextField("2");
+       pspasword.setBounds(30,80,200,20);
+*/
+
+}
+
+
+
+   
+    
+}
